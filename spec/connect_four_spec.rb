aff0d9680ac4887e9f0bcfa01642d87e3e33fc26
Adrian Perez <adrian1804perez@gmail.com>
2023-08-25 15:22:41 -0700

    Add tests for #add_black

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -16,6 +16,50 @@ describe ConnectFour do
       columns = row.length
       expect(columns).to eql(7)
     end
+  end
+
+  describe '#add_black' do
+    # Three different cases where the black symbol could be added
+    # 5th row indicates the bottom of the board
+    # 0th row indicates the top of the board
+
+    context 'when there is nothing at the bottom of the board' do
+      subject(:game_black) { described_class.new }
+      let(:column) { 5 }
+      let(:row) { 5 }
+      it 'symbol reaches the bottom' do
+        game_black.add_black(5)
+        square = game_black.board[column][row]
+        expect(square).to eq('⚫')
+      end
+    end
+
+    context 'when there is already a symbol occupying a square' do
+      subject(:game_black) { described_class.new }
+      let(:column) { 5 }
+      let(:row) { 4 }
+      before do
+        game_black.add_black(5)
+      end
+      it 'the symbol falls on top of the occupying square' do
+        game_black.add_black(5)
+        square = game_black.board[row][column]
+        expect(square).to eq('⚫')
+      end
+    end
+
+    context 'when there is no room left to add the symbol' do
+      subject(:game_black) { described_class.new }
+      before do
+        # To fill up the column (assuming previous tests pass)
+        6.times { game_black.add_black(5) }
+      end
+      it '#out_bounds_message is called' do
+        expect(game_black).to receive(:out_bounds_message).once
+        game_black.add_black(5)
+      end
+    end
+
 
   end
 end
