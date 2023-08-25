7502f09400e18cc1eb2255900490ecb2625feda4
Adrian Perez <adrian1804perez@gmail.com>
2023-08-25 11:02:46 -0700

    Add create_board

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
+# ConnectFour Class
 class ConnectFour
+  def create_board
+    Array.new(6) { Array.new(7) }
+  end
+
+  def initialize
+    @board = create_board
+  end
 end
