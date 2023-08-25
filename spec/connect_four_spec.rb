90106d07d67dd1b52db0c99f8983aa720968c9a8
Adrian Perez <adrian1804perez@gmail.com>
2023-08-25 15:52:45 -0700

    Add tests for #add_white

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -29,7 +29,7 @@ describe ConnectFour do
       let(:row) { 5 }
       it 'symbol reaches the bottom' do
         game_black.add_black(5)
-        square = game_black.board[column][row]
+        square = game_black.board[row][column]
         expect(square).to eq('⚫')
       end
     end
@@ -60,6 +60,46 @@ describe ConnectFour do
       end
     end
 
+    describe '#add_white' do
+      # Similar to the test for add_black
+      context 'when there is nothing on the bottom of the board' do
+        subject(:game_white) { described_class.new }
+        let(:column) { 3 }
+        let(:row) { 5 }
+        it 'reaches the bottom' do
+          game_white.add_white(3)
+          square = game_white.board[row][column]
+          expect(square).to eq('⚪')
+        end
+      end
 
+      context 'when there is already a symbol occupying a square' do
+        subject(:game_white) { described_class.new }
+        let(:column) { 3 }
+        let(:row) { 4 }
+        before do
+          game_white.add_white(3)
+        end
+
+        it 'symbol falls on top of the square being occupied' do
+          game_white.add_white(3)
+          square = game_white.board[row][column]
+          expect(square).to eq('⚪')
+        end
+      end
+
+      context 'when there is no room left to add the symbol' do
+        subject(:game_white) { described_class.new }
+        before do
+          # To fill up the column (assuming previous tests pass)
+          6.times { game_white.add_white(3) }
+        end
+
+        it '#out_of_bounds message is called' do
+          expect(game_white).to receive(:out_bounds_message).once
+          game_white.add_white(3)
+        end
+      end
+    end
   end
 end
