876e635581123b0e5c85db328f7a6156694b14b1
Adrian Perez <adrian1804perez@gmail.com>
2023-08-25 11:08:40 -0700

    Add show_board (No testing needed)

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -2,11 +2,23 @@
 
 # ConnectFour Class
 class ConnectFour
+  attr_accessor :board
+
   def create_board
     Array.new(6) { Array.new(7) }
   end
 
   def initialize
     @board = create_board
+  end
+
+  def show_board
+    for row in board 
+      for column in row
+        print "  #{column} " 
+      end
+      print "\n"
+    end
+    puts '  0  1  2  3  4  5  6  ' # The footer of the game
   end
 end
