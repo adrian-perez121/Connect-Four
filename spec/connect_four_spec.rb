a16f6be6654134f5fc295fc4a754267611ec42c7
Adrian Perez <adrian1804perez@gmail.com>
2023-08-25 11:02:30 -0700

    Add tests for create_board

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -1,3 +1,21 @@
 # frozen_string_literal: true
 
 require_relative '../lib/connect_four'
+
+describe ConnectFour do
+  describe '#create_board' do
+    subject(:game_board) { described_class.new }
+
+    it 'creates 6 rows' do
+      rows = game_board.create_board.length
+      expect(rows).to eql(6)
+    end
+
+    it 'creates 7 columns' do
+      row = game_board.create_board[0]
+      columns = row.length
+      expect(columns).to eql(7)
+    end
+
+  end
+end
