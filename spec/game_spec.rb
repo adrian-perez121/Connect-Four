d32de52f425d312a6fd2b2bc55c59b995f77be77
Adrian Perez <adrian1804perez@gmail.com>
2023-08-31 12:18:20 -0700

    Fix inconsistent naming

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -24,40 +24,40 @@ describe ConnectFour do
     # 0th row indicates the top of the board
 
     context 'when there is nothing at the bottom of the board' do
-      subject(:game_black) { described_class.new }
+      subject(:game_full) { described_class.new }
       let(:column) { 5 }
       let(:row) { 5 }
       it 'symbol reaches the bottom' do
-        game_black.add_full(5)
-        square = game_black.board[row][column]
+        game_full.add_full(5)
+        square = game_full.board[row][column]
         expect(square).to eq('♦')
       end
     end
 
     context 'when there is already a symbol occupying a square' do
-      subject(:game_black) { described_class.new }
+      subject(:game_full) { described_class.new }
       let(:column) { 5 }
       let(:row) { 4 }
       before do
-        game_black.add_full(5)
+        game_full.add_full(5)
       end
       it 'the symbol falls on top of the square being occupied' do
-        game_black.add_full(5)
-        square = game_black.board[row][column]
+        game_full.add_full(5)
+        square = game_full.board[row][column]
         expect(square).to eq('♦')
       end
     end
   
 
     context 'when there is no room left to add the symbol' do
-      subject(:game_black) { described_class.new }
+      subject(:game_full) { described_class.new }
       before do
         # To fill up the column (assuming previous tests pass)
-        6.times { game_black.add_full(5) }
+        6.times { game_full.add_full(5) }
       end
       it '#out_bounds_message is called' do
-        expect(game_black).to receive(:out_bounds_message).once
-        game_black.add_full(5)
+        expect(game_full).to receive(:out_bounds_message).once
+        game_full.add_full(5)
       end
     end
   end
@@ -65,41 +65,41 @@ describe ConnectFour do
   describe '#add_empty' do
     # Similar to the test for add_full
     context 'when there is nothing on the bottom of the board' do
-      subject(:game_white) { described_class.new }
+      subject(:game_empty) { described_class.new }
       let(:column) { 3 }
       let(:row) { 5 }
       it 'reaches the bottom' do
-        game_white.add_empty(3)
-        square = game_white.board[row][column]
+        game_empty.add_empty(3)
+        square = game_empty.board[row][column]
         expect(square).to eq('♢')
       end
     end
 
     context 'when there is already a symbol occupying a square' do
-      subject(:game_white) { described_class.new }
+      subject(:game_empty) { described_class.new }
       let(:column) { 3 }
       let(:row) { 4 }
       before do
-        game_white.add_empty(3)
+        game_empty.add_empty(3)
       end
 
       it 'symbol falls on top of the square being occupied' do
-        game_white.add_empty(3)
-        square = game_white.board[row][column]
+        game_empty.add_empty(3)
+        square = game_empty.board[row][column]
         expect(square).to eq('♢')
       end
     end
 
     context 'when there is no room left to add the symbol' do
-      subject(:game_white) { described_class.new }
+      subject(:game_empty) { described_class.new }
       before do
         # To fill up the column (assuming previous tests pass)
-        6.times { game_white.add_empty(3) }
+        6.times { game_empty.add_empty(3) }
       end
 
       it '#out_of_bounds message is called' do
-        expect(game_white).to receive(:out_bounds_message).once
-        game_white.add_empty(3)
+        expect(game_empty).to receive(:out_bounds_message).once
+        game_empty.add_empty(3)
       end
     end
   end
